c99c3c408f4dfc5b18f4e0915e3e8da487661b97
RUHUL AMIN <41443238+RUHULAMIN3856@users.noreply.github.com>
2018-11-06 14:34:41 +0600

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select*from tblruhul", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+            //SqlConnection con = new SqlConnection(@"data source =DESKTOP-SLFRI6N;database=AdoDB;Integrated security=True");
+            //con.Open();
+            //string query = "select*from dbo.tblruhul";
+            //SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            //DataTable dt = new DataTable();
+            //sda.Fill(dt);
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+            //con.Close();
+        }
+    }
+}
